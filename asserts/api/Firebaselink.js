1b8e7607adf3c0191ec4b415c2947dd6941d627f
kashanafzl <kashanafzal2221@gmail.com>
2023-02-19 12:09:21 +0500

    first commit

---
+++ asserts/api/Firebaselink.js
@@ -0,0 +1,18 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBHGD63zS18sPHb3HeoQmXu1OHS2wvLazE",
+    authDomain: "loginfirebase-a8a7d.firebaseapp.com",
+    projectId: "loginfirebase-a8a7d",
+    storageBucket: "loginfirebase-a8a7d.appspot.com",
+    messagingSenderId: "163170057085",
+    appId: "1:163170057085:web:0e36e9ccfab45c0855ea14"
+  };
+
+
+  firebase.initializeApp(firebaseConfig);
+
+  let db = firebase.firestore();
+
+
+  
---
+++ asserts/api/LoginAuth.js
@@ -0,0 +1,32 @@
+
+
+// function submit(){
+
+//     let email = document.getElementById('email').value;
+//     let password = document.getElementById('password').value;
+
+//     if(email === '' || password === ""){
+//         window.alert("please fill the data")
+//     } else{
+
+//         firebase.auth().signInWithEmailAndPassword(email, password)
+//         .then((userCredential) => {
+//           // Signed in
+//           var user = userCredential.user;
+          
+//           console.log(user)
+//           window.location.alert = ""
+//           // ...
+//         })
+//         .catch((error) => {
+//           var errorCode = error.code;
+//           var errorMessage = error.message;
+
+//           window.alert(errorMessage)
+//         });
+      
+
+        
+//     }
+// }
+
